2d3e56404371bffbea67b9b7394d1cf3c930b86e
Omar El Achgar <elachgaromar28@gmail.com>
2025-11-26 20:40:55 +0100

    Implémenter des modèles et définir les relations (User, Véhicule, Réservation).

---
+++ backEnd/app/Models/Vehicle.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vehicle extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'brand', 
+        'model', 
+        'year', 
+        'price_per_day', 
+        'description', 
+        'image', 
+        'is_available'
+    ];
+
+    public function reservations() {
+        return $this->hasMany(Reservation::class);
+    }
+}
