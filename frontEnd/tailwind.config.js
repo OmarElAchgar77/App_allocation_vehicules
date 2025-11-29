1915db44d6ad9acc1d8c10ea54817c40864850e6
sofyane0bentaleb <sofyane.bentaleb@hotmail.com>
2025-11-29 20:43:27 +0100

    react default files

---
+++ frontEnd/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Verify this path is correct for your project structure
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
