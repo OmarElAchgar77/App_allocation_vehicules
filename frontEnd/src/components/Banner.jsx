1bb9c7185b63084725df128d521d0a59202da101
sofyane0bentaleb <sofyane.bentaleb@hotmail.com>
2025-12-02 17:13:25 +0100

    add  Banner comp

---
+++ frontEnd/src/components/Banner.jsx
@@ -0,0 +1,20 @@
+function Banner() {
+  return (
+    <>
+      <section className="banner-section">
+        <div className="container">
+          <div className="banner-content">
+            <div className="banner-content__text">
+              <h2>Faites des économies considérables avec notre location de voiture à petit prix !</h2>
+              <p>
+                Principaux aéroports. Fournisseurs locaux. <span>24/7</span> Soutien.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Banner;
