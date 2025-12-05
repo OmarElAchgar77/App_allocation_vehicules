008f30b0a6a7909aa72a44f138944f6927b7ff4b
sofyane0bentaleb <sofyane.bentaleb@hotmail.com>
2025-12-05 01:54:31 +0100

    some npm debs

---
+++ frontEnd/src/theme.js
@@ -0,0 +1,6 @@
+// src/theme.js
+export const PRIMARY_COLOR = '#29476d';
+export const LIGHT_GRAY = '#f4f4f4';
+export const WHITE = '#ffffff';
+export const SUCCESS_COLOR = '#28a745';
+export const DANGER_COLOR = '#dc3545';
